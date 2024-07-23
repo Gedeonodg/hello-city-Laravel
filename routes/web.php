b11d2ff2dea70e82966f8bde9ebfd45d621ba1b8
Gedeonodg <154893753+Gedeonodg@users.noreply.github.com>
2024-07-23 21:57:16 +0000

    Make use of Route ::viex instead  of Route::get

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 use illuminate\Support\Facades\Route;
 
 
-Route::get("/", function(){
-    return view("pages/home");
-})->name("home");
-
+Route::get("/", fn() =>view("pages/home"))->name("home");
+Route :: view("/about-us",'pages.about')->name("about");
+/*
 Route::get("/about-us", function(){
     return view("pages.about");
 })->name("about");
+*/
